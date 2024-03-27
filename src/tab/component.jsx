150976596013bdf2cc1f5bcaf19cb42f914a0d84
Mikhail Goncharov <chack4993@gmail.com>
2024-03-27 21:04:20 +0300

    HW3

---
+++ src/tab/component.jsx
@@ -0,0 +1,8 @@
+export const Tab = ({ title, isActive, onClick }) => {
+  return (
+    <button onClick={onClick} disabled={isActive}>
+      {title}
+      {isActive && " - Active"}
+    </button>
+  );
+};
