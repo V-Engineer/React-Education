bac4fca24470b56265732de5c394e88f8a6e5e27
Mikhail Goncharov <chack4993@gmail.com>
2024-03-31 13:37:49 +0300

    HW4

--- src/dish/component.jsx
+++ src/dish/component.jsx
@@ -1,12 +1,22 @@
-import { Counter } from "../counter/component";
+import React from 'react';
+import { useCounter } from "../hooks/use-counter";
 
 export const Dish = ({dish}) =>{
+
+    const {counter, increment, decrement} = useCounter();
 
     return(
         <div>            
             <span>{dish.name}</span>
-            <Counter/>
+            
+
+            <div>
+                <button onClick={decrement} disabled={counter === 0}>-</button>
+                {counter}
+                <button onClick={increment} disabled={counter === 5}>+</button>
+            </div>
         </div>
+        
         
     );
 }
